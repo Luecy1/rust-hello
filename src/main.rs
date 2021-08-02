1faa819f1d1829b063635fb4bedeea004b1f9426
Youhei Oonuma <oonuma617@gmail.com>
2021-08-02 19:33:20 +0900

    借用の例

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
     // 所有権の移動
-    let x = String::from("Hello");
-    let y = x;
+    // let x = String::from("Hello");
+    // let y = x;
+    //
+    // println!("x :{}", x);
+    // println!("y :{}", y);
 
-    println!("x :{}", x);
-    println!("y :{}", y);
+    // 借用の例
+    let x = String::from("Hello");
+    println!("Hello no length {}", string_length(x));
+    println!("{}", x)    // Error
+}
+
+fn string_length(s: String) -> usize {
+    s.len()
 }

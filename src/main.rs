acf28a9e0eedec0c765b942aef8bed26c5266763
Youhei Oonuma <oonuma617@gmail.com>
2021-08-04 05:10:17 +0900

    mut

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,12 @@ fn main() {
     // println!("Hello no length {}", string_length(x));
     // println!("{}", x)    // Error
     println!("Hello no length {}", string_length(&x));
-    println!("{}", x)    // 借用
+    println!("{}", x);    // 借用
+
+    // 変数（）再定義
+    let mut x = 10; // mutをつけると変更ができる
+    x += 10;
+    println!("{}", x);
 }
 
 fn string_length(s: &String) -> usize {

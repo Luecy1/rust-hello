d0bfeef2812f8bffa61c8fcb29a8a2b5cfad3e68
Youhei Oonuma <oonuma617@gmail.com>
2021-08-05 00:45:00 +0900

    文字列の結合

--- src/syou4.rs
+++ src/syou4.rs
@@ -19,4 +19,23 @@ pub fn main() {
     println!("{}", moji);
     let moji = '\t';    // タブ
     println!("{}", moji);
+
+    // 文字列の一部を取り出す
+    let s = "hello Rust world";
+    let hello = &s[0..5];   // hello
+    println!("{}", hello);
+    let world = &s[11..];   // world
+    println!("{}", world);
+    let length = s.len();
+    println!("{}", length);      // 16
+
+    // 文字列の結合
+    let mut s = String::new();  // 空文字列を作る　変更可能のためmutをつける
+    s.push_str("hello ");
+    s.push_str("Rust ");
+    s.push_str("world ");
+    println!("{}", s);
+
+    let s = format!("{} {} {}", "hello", "rust", "world2");
+    println!("{}", s);
 }

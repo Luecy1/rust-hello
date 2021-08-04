7e7db3c9c1410e464dca1da8c862a296a126c49c
Youhei Oonuma <oonuma617@gmail.com>
2021-08-05 00:36:27 +0900

    特殊文字

--- src/syou4.rs
+++ src/syou4.rs
@@ -8,5 +8,15 @@ pub fn main() {
     let x = '😁';
     println!("{}", x);
 
-    
+    // 特殊文字
+    let moji = '\'';    // シングルクォート
+    println!("{}", moji);
+    let moji = '\\';    // バックスラッシュ
+    println!("{}", moji);
+    let moji = '\n';    // 改行
+    println!("{}", moji);
+    let moji = '\r';    // 復帰
+    println!("{}", moji);
+    let moji = '\t';    // タブ
+    println!("{}", moji);
 }

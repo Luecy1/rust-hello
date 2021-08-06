cbcf93d654b1b5ce2db475c287b3e2874b47443f
Youhei Oonuma <oonuma617@gmail.com>
2021-08-06 09:18:17 +0900

    整数と実数

--- src/syou5.rs
+++ src/syou5.rs
@@ -1,5 +1,4 @@
 pub fn main() {
-
     // セミコロンが文の区切り
     let a = 10;
     let b = 20;
@@ -16,6 +15,13 @@ pub fn main() {
     if a > 20 {
         println!("a is over 20");
     }
+
+    // 割り算と整数、実数の違い
+    let ans = 10 / 3; // ans = 3
+    println!("{}", ans);
+
+    let ans = 10.0 / 3.0; // ans = 3.3333333333333335
+    println!("{}", ans);
 }
 
 fn add(a: i32, b: i32) -> i32 {

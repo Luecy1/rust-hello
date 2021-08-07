3ea82b1fd53ad22d273a276b24943b2731f59ac6
Youhei Oonuma <oonuma617@gmail.com>
2021-08-07 18:00:30 +0900

    いろいろ

--- src/syou5.rs
+++ src/syou5.rs
@@ -22,8 +22,56 @@ pub fn main() {
 
     let ans = 10.0 / 3.0; // ans = 3.3333333333333335
     println!("{}", ans);
+
+    // ビット演算
+    let a: u8 = 0b1111;
+    let b: u8 = 0b0011;
+    println!("a & b = {:04b}", a & b);
+    println!("a | b = {:04b}", a | b);
+
+    // シフト演算子
+    let a: u8 = 0x02;
+    println!("a << 1 = {}", a << 1);
+    println!("a >> 1 = {}", a >> 1);
+
+    // 論理演算子
+    let a = true;
+    let b = false;
+    println!(" a && b = {}", a && b);
+    println!(" a || b = {}", a || b);
+
+    no_param();
+    one_param(1);
+    two_param(2, 3);
+    println!("{}", return_value());
+    str_param("hoge")
 }
 
 fn add(a: i32, b: i32) -> i32 {
     a + b
+}
+
+// 引数も返り値もない関数
+fn no_param() {
+    println!("no_param called");
+}
+
+// 引数が一つの関数
+fn one_param(x: i32) {
+    println!("{}", x);
+}
+
+// 引数が2つの関数
+fn two_param(x: i32, y: i32) {
+    println!("x y {} {}", x, y);
+}
+
+// 戻り値がある関数
+fn return_value() -> i32 {
+    10
+}
+
+// 文字列を受け取る関数
+fn str_param(s: &str) {
+    println!("called {}", s);
 }

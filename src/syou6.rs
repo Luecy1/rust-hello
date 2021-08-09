7da423289370b0a40f89a2e953db0b6dbf537e80
Youhei Oonuma <oonuma617@gmail.com>
2021-08-09 12:42:27 +0900

    for

--- src/syou6.rs
+++ src/syou6.rs
@@ -14,4 +14,27 @@ pub fn main() {
         "a < b"
     };
     println!("{}", x);
+
+    // 繰り返し
+    let vec = vec![10, 20, 30, 40, 50];
+    for v in &vec {
+        print!("{} ", v);
+        let vp: i32 = *v;   // *をつけて参照外し
+    }
+    println!();
+
+    // イテレータを使う
+    for i in vec.iter() {
+        println!("{}", i);
+    }
+
+    // インデックス付きでループ タプルで受け取る
+    for (i, x) in vec.iter().enumerate() {
+        println!("index:{} content{}", i, x);
+    }
+
+    // 指定数ループ
+    for i in 0..10 {
+        println!("{}", i);
+    }
 }

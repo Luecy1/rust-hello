7212e446f34991285c7243d05a8945482c2869d1
Youhei Oonuma <oonuma617@gmail.com>
2021-08-09 12:30:01 +0900

    if

--- src/syou6.rs
+++ src/syou6.rs
@@ -1,1 +1,17 @@
-pub fn main() {}
+pub fn main() {
+    let a = 10;
+    let b = 20;
+    if a < b {
+        println!("a > b is true");
+    } else {
+        println!("a > b is ng")
+    }
+
+    // ifは値を返す
+    let x = if a < b {
+        "a > b"
+    } else {
+        "a < b"
+    };
+    println!("{}", x);
+}

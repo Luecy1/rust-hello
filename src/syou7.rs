13d0c5ce06a7c99d9b3c3be6cfcfc2529835e84b
Youhei Oonuma <oonuma617@gmail.com>
2021-08-11 03:32:33 +0900

    配列

--- src/syou7.rs
+++ src/syou7.rs
@@ -1,3 +1,26 @@
 pub fn main() {
-    println!("syou7")
+    let array = [1, 2, 3, 4, 5];
+    println!("{}", array[0]);
+    println!("{}", array[4]);
+    println!("{}", array.len());
+
+    // 文字列の配列
+    let array = ["one", "two", "three", "four", "five"];
+    println!("{}", array[0]);
+    println!("{}", array[4]);
+    println!("{}", array.len());
+
+    // 型を指定するときは要素数も指定する
+    let array: [u8; 5] = [0x10, 0x20, 0x30, 0x40, 0x50];
+    println!("{}", array[0]);
+    println!("{}", array[4]);
+    println!("{}", array.len());
+
+    // 0で初期化する
+    let mut array: [u8; 16] = [0; 16];
+    print!("0 init ->");
+    for x in array {
+        print!("{} ", x);
+    }
+    println!()
 }

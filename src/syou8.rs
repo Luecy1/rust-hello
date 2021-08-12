a828e2c0b1098b84c8c4b2e7d3f0ab23bff659fc
Youhei Oonuma <oonuma617@gmail.com>
2021-08-13 00:30:01 +0900

    構造体

--- src/syou8.rs
+++ src/syou8.rs
@@ -1,1 +1,32 @@
-pub fn main() {}
+pub fn main() {
+    // Personのインスタンス化
+    let person = Person {
+        id: 100,
+        name: String::from("tanaka"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+
+    println!("Person.id {}", person.id);
+    println!("Person.name {}", person.name);
+    println!("Person.age {}", person.age);
+    println!("Person.addr {}", person.addr);
+
+    // 関数呼び出しで構造体を渡す
+    print_person(&person);
+}
+
+// 関数呼び出しで構造体を渡す
+fn print_person(person: &Person) {
+    println!("Person.id {}", person.id);
+    println!("Person.name {}", person.name);
+    println!("Person.age {}", person.age);
+    println!("Person.addr {}", person.addr);
+}
+
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    addr: String,
+}

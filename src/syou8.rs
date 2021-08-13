dc4ef104cd7fdee9a6755007bab3a21a8e268df5
Youhei Oonuma <oonuma617@gmail.com>
2021-08-13 09:05:26 +0900

    構造体を変更できる関数の定義、呼び出し

--- src/syou8.rs
+++ src/syou8.rs
@@ -14,6 +14,20 @@ pub fn main() {
 
     // 関数呼び出しで構造体を渡す
     print_person(&person);
+
+    // Personのインスタンス化(変更可能)
+    let mut person = Person {
+        id: 100,
+        name: String::from("tanaka"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+    // 構造体を変更可能にする関数の呼び出し
+    add_age(&mut person);
+    println!("Person.id {}", person.id);
+    println!("Person.name {}", person.name);
+    println!("Person.age {}", person.age);
+    println!("Person.addr {}", person.addr);
 }
 
 // 関数呼び出しで構造体を渡す
@@ -22,6 +36,11 @@ fn print_person(person: &Person) {
     println!("Person.name {}", person.name);
     println!("Person.age {}", person.age);
     println!("Person.addr {}", person.addr);
+}
+
+// 構造体の中身を変更できる関数
+fn add_age(person: &mut Person) {
+    person.age += 1;
 }
 
 struct Person {

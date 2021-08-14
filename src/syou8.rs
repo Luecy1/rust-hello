108123a7585821f5228ea16b80a837aa4259c15f
Youhei Oonuma <oonuma617@gmail.com>
2021-08-15 02:34:17 +0900

    構造体

--- src/syou8.rs
+++ src/syou8.rs
@@ -28,6 +28,18 @@ pub fn main() {
     println!("Person.name {}", person.name);
     println!("Person.age {}", person.age);
     println!("Person.addr {}", person.addr);
+
+    // 戻り値が構造体の関数を呼び出す
+    let person2 = new_person(1, "hoge");
+    print_person(&person2);
+
+    // ベクタで構造体を扱う
+    let mut vec_person = vec![new_person(1, "vec1"), new_person(2, "vec2")];
+    vec_person.push(new_person(3, "3ninme"));
+    for person in &vec_person {
+        println!("-------");
+        print_person(person);
+    }
 }
 
 // 関数呼び出しで構造体を渡す
@@ -41,6 +53,16 @@ fn print_person(person: &Person) {
 // 構造体の中身を変更できる関数
 fn add_age(person: &mut Person) {
     person.age += 1;
+}
+
+// 構造体が戻り値の関数
+fn new_person(id: i32, name: &str) -> Person {
+    Person {
+        id,
+        name: name.to_string(),
+        age: -1,
+        addr: "Unknown".to_string(),
+    }
 }
 
 struct Person {

0b519c26440cd60b773c1fb9520c0817ff4f2975
Youhei Oonuma <oonuma617@gmail.com>
2021-08-21 21:39:08 +0900

    ジェネリクスを使った関数

--- src/Syou9.rs
+++ src/Syou9.rs
@@ -3,6 +3,16 @@ pub fn main() {
     let mut v: Vec<&str> = Vec::new();
     v.push("aiueo");
     println!("{:?}", v);
+
+    let v = [1, 2, 3, 4, 5];
+    print(&v);
 }
 
 // ジェネリクスを使った関数
+fn print<T>(a: &[T]) where T: std::fmt::Debug { // whereでトレイトを指定する
+    print("a is");
+    for x in a {
+        print!("{:?}", x);
+    }
+    println!();
+}

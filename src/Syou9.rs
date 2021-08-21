773a12fb7429e62ab9987224faca394bf615e48a
Youhei Oonuma <oonuma617@gmail.com>
2021-08-21 21:56:50 +0900

    トレイト

--- src/Syou9.rs
+++ src/Syou9.rs
@@ -6,6 +6,18 @@ pub fn main() {
 
     let v = [1, 2, 3, 4, 5];
     print(&v);
+
+    let rect = Rectangle {
+        width: 10.0,
+        height: 20.0,
+    };
+    let tri = Triangle {
+        width: 10.0,
+        height: 20.0,
+    };
+    let cir = Circle {
+        radius: 10.0,
+    };
 }
 
 // ジェネリクスを使った関数
@@ -15,4 +27,43 @@ fn print<T>(a: &[T]) where T: std::fmt::Debug { // whereでトレイトを指定
         print!("{:?}", x);
     }
     println!();
+}
+
+// 四角形
+struct Rectangle {
+    width: f32,
+    height: f32,
+}
+
+// 三角形
+struct Triangle {
+    width: f32,
+    height: f32,
+}
+
+// 円
+struct Circle {
+    radius: f32,
+}
+
+trait CalcArea {
+    fn calc_area(&self) -> f32;
+}
+
+impl CalcArea for Rectangle {
+    fn calc_area(&self) -> f32 {
+        self.width * self.height
+    }
+}
+
+impl CalcArea for Triangle {
+    fn calc_area(&self) -> f32 {
+        self.width * self.height * 0.5
+    }
+}
+
+impl CalcArea for Circle {
+    fn calc_area(&self) -> f32 {
+        self.radius * self.radius * 3.14
+    }
 }
